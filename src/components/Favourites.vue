cef0bb9301ef935eacf544a18145c1fc68296907
Ran Guin <rguin@iMac.local>
2021-01-04 20:30:12 -0800

    update favourites page

--- src/components/Favourites.vue
+++ src/components/Favourites.vue
@@ -4,7 +4,8 @@
       h2 Favourites
     v-card-text
       v-container.padded
-        h2 Personal Favourites details...
+        h2 No Favourites yet registered
+        h4 (Note: this functionality may still be pending)
 
 </template>
 <script>
